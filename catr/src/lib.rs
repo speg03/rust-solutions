b85764de0e99aa2d45c27a36b9e79184338ed0db
Takahiro Yano <speg03@gmail.com>
2024-06-03 22:12:24 +0900

    引数の定義（自作版）

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,8 +1,51 @@
+use clap::{App, Arg};
 use std::error::Error;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Takahiro Yano <speg03@gmail.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .default_value("-")
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("number_nonblank_lines")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number nonblank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
 }
